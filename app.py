941f7942575f270380033cb3d7baa241b7c6359a
Miquel Marin <mmarin.bits@gmail.com>
2022-12-04 20:20:55 +0100

    Try GET/POST

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    elif request.method == "POST":
+        name = request.form.get("name", "World")
+        return render_template("greet.html", name=name)
+
+# flask --debug run
